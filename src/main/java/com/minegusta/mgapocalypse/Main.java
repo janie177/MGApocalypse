e3e9756b3b34e244cb623c67925958d142130a94
Jan <janie177@hotmail.com>
2014-11-13 14:04:18 +0100

    First push :D

---
+++ src/main/java/com/minegusta/mgapocalypse/Main.java
@@ -0,0 +1,39 @@
+package com.minegusta.mgapocalypse;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+
+    public static Plugin PLUGIN;
+
+    @Override
+    public void onEnable()
+    {
+        //Set Plugin
+        PLUGIN = this;
+
+
+        //Config files
+
+
+        //Listeners
+
+
+        //Commands
+
+
+        //Tasks
+
+
+    }
+
+
+    @Override
+    public void onDisable()
+    {
+        //Cancel Tasks
+
+    }
+}
