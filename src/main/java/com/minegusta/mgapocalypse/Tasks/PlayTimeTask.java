525774ea16f19e6e1ce172fea9b95c2dc0577dbb
Jan <janie177@hotmail.com>
2015-07-06 19:19:50 +0200

    Optimized the play time task.

--- src/main/java/com/minegusta/mgapocalypse/Tasks/PlayTimeTask.java
+++ src/main/java/com/minegusta/mgapocalypse/Tasks/PlayTimeTask.java
@@ -2,6 +2,7 @@ package com.minegusta.mgapocalypse.Tasks;
 
 import com.minegusta.mgapocalypse.MGApocalypse;
 import com.minegusta.mgapocalypse.Main;
+import com.minegusta.mgapocalypse.files.MGPlayer;
 import org.bukkit.Bukkit;
 
 public class PlayTimeTask
@@ -9,17 +10,13 @@ public class PlayTimeTask
     private static int id = -1;
 
     public static void start() {
-        id = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.PLUGIN, () ->
-        {
-            MGApocalypse.getMGPlayers().stream().forEach(p ->
-            {
-                if(p.getIfPlaying())
-                {
-                    p.addPlayTime(1);
-                    p.addTimeAlive(1);
-                }
-            });
-        },20 * 60, 20 * 60);
+        id = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.PLUGIN, () -> MGApocalypse.getMGPlayers().stream().filter(MGPlayer::getIfPlaying).forEach(PlayTimeTask::updateTime),20 * 60, 20 * 60);
+    }
+
+    private static void updateTime(MGPlayer p)
+    {
+        p.addPlayTime(1);
+        p.addTimeAlive(1);
     }
 
     public static void stop()
