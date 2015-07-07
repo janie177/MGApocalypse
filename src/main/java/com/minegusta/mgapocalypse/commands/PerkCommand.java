742cdf3ce75b22c1c8506da4265186df41abbcac
Jan <janie177@hotmail.com>
2015-07-07 03:02:56 +0200

    Added a command to open the perk interface.

--- src/main/java/com/minegusta/mgapocalypse/commands/PerkCommand.java
+++ src/main/java/com/minegusta/mgapocalypse/commands/PerkCommand.java
@@ -1,14 +1,25 @@
 package com.minegusta.mgapocalypse.commands;
 
+import com.minegusta.mgapocalypse.util.PerkMenu;
+import com.minegusta.mgapocalypse.util.WorldCheck;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class PerkCommand implements CommandExecutor
 {
     @Override
     public boolean onCommand(CommandSender s, Command command, String label, String[] args)
     {
+        if(!(s instanceof Player))return true;
+
+        Player p = (Player) s;
+
+        if(!WorldCheck.is(p.getWorld()))return true;
+
+        p.openInventory(PerkMenu.build(p));
+
         return true;
     }
 
