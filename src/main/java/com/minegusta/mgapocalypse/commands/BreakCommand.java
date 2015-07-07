a46ce215848b59d6182e31d61782892ebdd1e59f
Jan <janie177@hotmail.com>
2015-07-07 18:13:54 +0200

    You cannot break when not playing.

--- src/main/java/com/minegusta/mgapocalypse/commands/BreakCommand.java
+++ src/main/java/com/minegusta/mgapocalypse/commands/BreakCommand.java
@@ -5,6 +5,7 @@ import com.minegusta.mgapocalypse.MGApocalypse;
 import com.minegusta.mgapocalypse.files.MGPlayer;
 import com.minegusta.mgapocalypse.util.Break;
 import com.minegusta.mgapocalypse.util.WorldCheck;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -20,6 +21,13 @@ public class BreakCommand implements CommandExecutor {
 
         MGPlayer mgp = MGApocalypse.getMGPlayer(p);
         String uuid = p.getUniqueId().toString();
+
+        if(!mgp.getIfPlaying())
+        {
+            p.sendMessage(ChatColor.YELLOW + "You are already on a break!");
+            p.sendMessage(ChatColor.YELLOW + "Please use a Wasteland join sign to play.");
+            return true;
+        }
 
         if (!MGPlayer.breaks.containsKey(uuid)) {
             Break b = new Break(p);
