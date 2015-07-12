aeeed40e1a07f33a9c27fcda91f23a478f45b11a
Jan <janie177@hotmail.com>
2015-07-12 07:34:46 +0200

    Diamonds are more rare. Grenades are more rare.

--- src/main/java/com/minegusta/mgapocalypse/lootblocks/Loot.java
+++ src/main/java/com/minegusta/mgapocalypse/lootblocks/Loot.java
@@ -4,8 +4,8 @@ import com.minegusta.mgapocalypse.util.RandomNumber;
 import com.minegusta.mgloot.loottables.LootItem;
 
 public class Loot {
-    private static final LootItem[] grave = {LootItem.SKULL, LootItem.BONE, LootItem.GRENADE, LootItem.SNOWBALL, LootItem.ARROW_4,};
-    private static final LootItem[] ore = {LootItem.COAL, LootItem.COAL, LootItem.COAL, LootItem.COAL, LootItem.COAL, LootItem.COAL, LootItem.COAL, LootItem.COAL, LootItem.IRONBAR, LootItem.IRONBAR, LootItem.IRONBAR, LootItem.IRONBAR, LootItem.DIAMOND};
+    private static final LootItem[] grave = {LootItem.SKULL, LootItem.SKULL, LootItem.BONE, LootItem.GRENADE, LootItem.SNOWBALL, LootItem.ARROW_4, LootItem.STONESWORD, LootItem.BOW_WEAK};
+    private static final LootItem[] ore = {LootItem.COAL, LootItem.COAL, LootItem.COAL, LootItem.COAL, LootItem.COAL, LootItem.COAL, LootItem.COAL, LootItem.COAL, LootItem.IRONBAR,LootItem.COAL, LootItem.IRONBAR,LootItem.COAL, LootItem.IRONBAR, LootItem.IRONBAR, LootItem.IRONBAR, LootItem.IRONBAR, LootItem.DIAMOND};
 
     public static LootItem getOre() {
         return ore[RandomNumber.get(ore.length) - 1];
