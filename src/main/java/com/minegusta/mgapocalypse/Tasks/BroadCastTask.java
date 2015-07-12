82b51c3e3914ccc44d4c0fcb4f6819ed61b942f4
Jan <janie177@hotmail.com>
2015-07-12 17:26:32 +0200

    Added some broadcast messages to wasteland.

--- src/main/java/com/minegusta/mgapocalypse/Tasks/BroadCastTask.java
+++ src/main/java/com/minegusta/mgapocalypse/Tasks/BroadCastTask.java
@@ -7,11 +7,13 @@ import org.bukkit.ChatColor;
 public class BroadCastTask {
     private static final String[] messages =
             {
-                    "Need a world map? " + ChatColor.DARK_BLUE + "http://www.minegusta.com/map.php",
+                    "Need a world map? " + ChatColor.BLUE + "http://www.minegusta.com/map.php",
                     "You are playing Wasteland! To return to the hub, use: " + ChatColor.YELLOW + "/Hub",
                     "To see your stats, use : " + ChatColor.YELLOW + "/Menu",
                     "Upgrade your perks! Use: " + ChatColor.YELLOW + "/Perk",
-                    "Want cool donor perks? " + ChatColor.YELLOW + "http://store.minegusta.com"
+                    "Want cool donor perks? " + ChatColor.YELLOW + "http://store.minegusta.com",
+                    "Vote for the server to earn a perkpoint! " + ChatColor.YELLOW + "http://www.minegusta.com/vote.php",
+                    "Need help with the game? " + ChatColor.YELLOW + "http://minegusta.wikia.com/wiki/Wasteland"
             };
     private static int index = 0;
 
@@ -21,7 +23,7 @@ public class BroadCastTask {
             Bukkit.getOnlinePlayers().forEach(p ->
             {
                 p.sendMessage(ChatColor.RED + "- - - " + ChatColor.GOLD + "WasteLand" + ChatColor.RED + " - - -");
-                p.sendMessage(messages[index]);
+                p.sendMessage(ChatColor.LIGHT_PURPLE + messages[index]);
                 p.sendMessage(ChatColor.RED + "- - - - - - - - -");
                 index++;
             });
