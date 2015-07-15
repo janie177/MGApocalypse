eafdab8e78f9fa63027d1dd9bc40ea796e1a83d2
Jan <janie177@hotmail.com>
2015-07-15 22:45:38 +0200

    Items are now more damaged.

--- src/main/java/com/minegusta/mgapocalypse/util/DamageItem.java
+++ src/main/java/com/minegusta/mgapocalypse/util/DamageItem.java
@@ -8,7 +8,7 @@ public class DamageItem {
      */
     public static short damage(short s) {
         int start = (int) s;
-        int end = (start / 10) * RandomNumber.get(3, 7);
+        int end = (start / 10) * RandomNumber.get(5, 8);
         return (short) end;
     }
 }
