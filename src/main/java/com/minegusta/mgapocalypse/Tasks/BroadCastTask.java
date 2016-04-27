4ca6868b93dca3a174fc2449f8d78d2a9bd3c0d8
Jan <janie177@hotmail.com>
2016-04-27 23:15:08 +0200

    Fixed broadcast task messages. Added latest MGLoot.

--- src/main/java/com/minegusta/mgapocalypse/Tasks/BroadCastTask.java
+++ src/main/java/com/minegusta/mgapocalypse/Tasks/BroadCastTask.java
@@ -7,13 +7,12 @@ import org.bukkit.ChatColor;
 public class BroadCastTask {
     private static final String[] messages =
             {
-                    "Need a world map? " + ChatColor.BLUE + "http://www.minegusta.com/map.php",
+                    "Need a world map? " + ChatColor.BLUE + "http://www.minegusta.net/wasteland.php",
                     "You are playing Wasteland! To return to the hub, use: " + ChatColor.YELLOW + "/Hub",
                     "To see your stats, use : " + ChatColor.YELLOW + "/Menu",
                     "Upgrade your perks! Use: " + ChatColor.YELLOW + "/Perk",
-                    "Want cool donor perks? " + ChatColor.YELLOW + "http://store.minegusta.com",
-                    "Vote for the server to earn a perkpoint! " + ChatColor.YELLOW + "http://www.minegusta.com/vote.php",
-                    "Need help with the game? " + ChatColor.YELLOW + "http://minegusta.wikia.com/wiki/Wasteland"
+                    "Want cool donor perks? " + ChatColor.YELLOW + "http://store.minegusta.net",
+                    "Vote for the server to earn a perkpoint! " + ChatColor.YELLOW + "http://www.minegusta.net/vote.php"
             };
     private static int index = 0;
 
